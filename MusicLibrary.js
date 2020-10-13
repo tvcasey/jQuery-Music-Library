81661ae3e2697601c2d1d8e8fb1f4500f43bfe7b
Timothy Casey <tvcasey@yahoo.com>
2020-10-12 21:21:55 -0400

    Fixed the Ajax to append the information according to one variable at the table body, instead of sending the five topics through a loop each.  That made me have issues with table fields not being active.  Now I can manipulate the data.  Will try to see if I can organize answers to questions tommorow.

--- MusicLibrary.js
+++ MusicLibrary.js
@@ -12,12 +12,20 @@ function grabMusic() {
         success: function (data, textStatus, jQxhr) {
             //runs if API is successful
             console.log(data);
+            $("#tableBody").html("");
             for(let i = 0; i < data.length; i++){
-            $("#musicTitles").append("<tr><td>" + data[i].title + "</tr></td>");
-            $("#musicAlbums").append("<tr><td>" + data[i].album + "</tr></td>");
-            $("#musicArtists").append("<tr><td>" + data[i].artist + "</tr></td>");
-            $("#musicGenres").append("<tr><td>" + data[i].genre + "</tr></td>");
-            $("#musicReleaseDates").append("<tr><td>" + data[i].releaseDate + "</tr></td>");
+                $("#tableBody").append("<tr>"
+                    +"<td>" + data[i].title + "</td>"
+                    +"<td>" + data[i].album + "</td>"
+                    +"<td>" + data[i].artist + "</td>"
+                    +"<td>" + data[i].genre + "</td>"
+                    +"<td>" + data[i].releaseDate + "</td>"
+                +"</tr>");
+            //$("#musicTitles").append("<tr><td>" + data[i].title + "</tr></td>");
+            //$("#musicAlbums").append("<tr><td>" + data[i].album + "</tr></td>");
+            //$("#musicArtists").append("<tr><td>" + data[i].artist + "</tr></td>");
+            //$("#musicGenres").append("<tr><td>" + data[i].genre + "</tr></td>");
+            //$("#musicReleaseDates").append("<tr><td>" + data[i].releaseDate + "</tr></td>");
             }
         },
         error: function (jQxhr, textStatus, errorThrown) {
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # jQuery-Music-Library
+
+I used my method for the jQuery Ajax with an API and it tied the append to each topic found
+on the tbody, which was I believe five.  Tony showed me a real simple, clever way to sync
+the data by putting the information each to their appropriate cell while using the tbody variable
+as the only variable necessary to put the information in the table properly.  Now everything lines up right and has the right padding.  The data can be organized now and I may attempt to do so tommorow.  Just to see if it is easy or not.  Tim Casey.
